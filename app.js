5191b139fd0852d4ebeea7d76e59c79c1679dab8
Kashif Roshen <kashif@wisenet.co>
2019-11-03 10:03:33 +0530

    moved routes to a separate file and injected the book model

--- app.js
+++ app.js
@@ -4,58 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
-// books - get all
-bookRouter.route('/books')
-  .post((request, response) => {
-    const book = new Book(request.body);
-    book.save();
-
-    return response.status(201).json(book);
-  })
-  .get((request, response) => {
-    let query = {};
-
-    if (request.query.genre) {
-      query = {
-        genre: request.query.genre
-      };
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return response.send(err);
-      }
-
-      return response.json(books);
-    });
-  });
-
-// books - get by id
-bookRouter.route('/books/:bookId')
-  .get((request, response) => {
-    const {
-      bookId
-    } = request.params;
-
-    Book.findById(bookId, (err, book) => {
-      if (err) {
-        return response.send(err);
-      }
-
-      return response.json(book);
-    });
-  });
-
-app
-  .use('/api', bookRouter);
+// api routes
+app.use('/api', bookRouter);
 
 // handling the base URL get requests
 app.get('/', (request, response) => {
